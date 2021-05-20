fce7268f7642184d923abc81ef0bfe16266e4bd8
TiKsers <66319554+TiKsers@users.noreply.github.com>
2021-05-20 11:10:18 +0200

    Add files via upload

---
+++ GoodEmployee.java
@@ -0,0 +1,25 @@
+public class GoodEmployee {
+    private String firstname;
+    private String lastName;
+
+    public GoodEmployee(String firstname, String lastName) {
+        this.firstname = firstname;
+        this.lastName = lastName;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
